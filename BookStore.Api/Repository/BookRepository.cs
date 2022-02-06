92575a08b15173c04dbd50f5d93ba2a88b0bc70c
hongthang152 <nguyenhongthang1998@gmail.com>
2022-02-06 15:47:04 -0500

    Add view books feature

--- BookStore.Api/Repository/BookRepository.cs
+++ BookStore.Api/Repository/BookRepository.cs
@@ -2,6 +2,7 @@
 using BookStore.Api.Models;
 using Dapper;
 using Npgsql;
+using System.Text;
 
 namespace BookStore.Api.Repository
 {
@@ -23,12 +24,75 @@ namespace BookStore.Api.Repository
             return true;
         }
 
-        public async Task<Book?> GetBook(string isbn)
+        public async Task<List<Book>?> GetBooks(string isbn, string genre, string sortBy, string orderBy, int offset, int limit)
         {
             //Get book from database
-            var results = await _connection.QueryAsync<Book>($"SELECT * FROM book b " +
-                                                             $"WHERE b.isbn = '{isbn}' ");
-            return results.FirstOrDefault();
+            StringBuilder queryBuilder = new StringBuilder("SELECT * FROM book b ");
+            StringBuilder filterBuilder = new StringBuilder();
+            
+            Dictionary<string, string> filter = new Dictionary<string, string>();
+            filter.Add("isbn", isbn);
+            filter.Add("genre", genre);
+            foreach (KeyValuePair<string, string> entry in filter)
+            {
+                if(entry.Value != null)
+                {
+                    if(filterBuilder.Length == 0)
+                    {
+                        filterBuilder.Append("WHERE ");
+                    }
+                    else
+                    {
+                        filterBuilder.Append("AND ");
+                    }
+                    if(!String.IsNullOrEmpty(entry.Value))
+                    {
+                        filterBuilder.Append($"{entry.Key} = '{entry.Value}' ");
+                    }
+                }
+            }
+
+            
+
+            StringBuilder sortByStringBuilder = new StringBuilder();
+            if (sortBy != null)
+            {
+                sortByStringBuilder.Append($"ORDER BY {sortBy} ");
+                if (String.IsNullOrEmpty(orderBy))
+                {
+                    sortByStringBuilder.Append("ASC ");
+                } 
+                else
+                {
+                    sortByStringBuilder.Append($"{orderBy} ");
+                }
+            }
+
+            StringBuilder offsetBuilder = new StringBuilder();
+            if(offset != 0)
+            {
+                offsetBuilder.Append($"OFFSET {offset} ");
+            }
+
+            queryBuilder.Append(filterBuilder).Append(sortByStringBuilder).Append(offsetBuilder);
+            if (limit != 0)
+            {
+                queryBuilder.Append($"LIMIT {limit} ");
+            }
+
+            var results = await _connection.QueryAsync<Book>(queryBuilder.ToString());
+            return results.ToList();
+        }
+
+        public async Task<List<Book>?> GetBooksByRating(decimal stars)
+        {
+            var results = await _connection.QueryAsync<Book>(
+                "SELECT b.*, AVG(r.stars) as rating FROM book b " +
+                "JOIN RATING r ON r.isbn = b.isbn GROUP BY b.isbn " +
+                $"HAVING AVG(r.stars) >= {stars} " +
+                "ORDER BY rating ASC "
+                );
+            return results.ToList();
         }
     }
 }
