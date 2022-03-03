2122df299df6cd003a3bc64256b1ebd96da40344
PetersonNormil <petersonnormil@gmail.com>
2022-03-03 09:18:45 -0500

    commit

--- BookStore.Api/Interface/IBookRepository.cs
+++ BookStore.Api/Interface/IBookRepository.cs
@@ -8,5 +8,6 @@ namespace BookStore.Api.Interface
         Task<List<Book>?> GetBooks(string isbn, string genre, string sortBy, string orderBy, int offset, int limit);
         Task<List<Book>?> GetBooksByRating(decimal stars);
         Task<bool> AddBook(Book book);
+       // Task<Book?> GetBook(string isbn);
     }
 }
