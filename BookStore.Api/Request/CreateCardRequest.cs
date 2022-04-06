b89e8bf27132b846805ffb2c17862e2bea00fb1a
AlexandrDream <98363672+AlexandrDream@users.noreply.github.com>
2022-04-06 13:17:26 -0400

    Add files via upload

---
+++ BookStore.Api/Request/CreateCardRequest.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookStore.Api.Request
+{
+    public class CreateCardRequest
+    {
+        [Required(AllowEmptyStrings = false)]
+        public int user_id { get; set; }
+        [Required(AllowEmptyStrings = false)]
+        public string name { get; set; }
+        [Required(AllowEmptyStrings = false)]
+        public string number { get; set; }
+        [Required(AllowEmptyStrings = false)]
+        public string expire_date { get; set; }
+        [Required(AllowEmptyStrings = false)]
+        public int pin { get; set; }
+    }
+}
---
+++ BookStore.Api/Request/UpdateUserInfo.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookStore.Api.Request
+{
+    public class UpdateUserInfo
+    {
+        [Required(AllowEmptyStrings = false)]
+        public string? username { get; set; }
+
+        [Required(AllowEmptyStrings = true)]
+        public string? password { get; set; }
+
+        [Required(AllowEmptyStrings = true)]
+        public string? name { get; set; }
+
+        [Required(AllowEmptyStrings = true)]
+        public string? email { get; set; }
+
+        [Required(AllowEmptyStrings = true)]
+        public string? home_address { get; set; }
+    }
+}
