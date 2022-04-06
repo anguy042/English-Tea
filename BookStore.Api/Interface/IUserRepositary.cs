8626040c8a5dcb9c2e6d710aa9ccfd03ecbef828
AlexandrDream <98363672+AlexandrDream@users.noreply.github.com>
2022-04-06 13:14:50 -0400

    Add files via upload

---
+++ BookStore.Api/Interface/IUserRepositary.cs
@@ -0,0 +1,14 @@
+﻿using BookStore.Api.Models;
+using System.Threading.Tasks;
+
+namespace BookStore.Api.Interface
+{
+    public interface IUserRepository
+    {
+        Task<IEnumerable<User?>> Get(string username);
+        Task<IEnumerable<CreditCard?>> GetCard(int user_id);
+        Task Create(string username, string password, string name, string email, string home_address);
+        Task Update(string username, string password, string name, string email, string home_address);
+        Task CreateCard(int user_id, string name, string number, string expire_date, int pin);
+    }
+}
